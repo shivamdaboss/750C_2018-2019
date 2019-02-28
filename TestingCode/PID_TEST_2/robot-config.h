747c8b1be31ac493e3326413e7bdfeb25edac90f
Shivam Agrawal <agrawalshivam1119@gmail.com>
2019-02-28 12:20:16 -0500

    Update and rename PID_TEST_2.vex to TestingCode/PID_TEST_2/robot-config.h

---
+++ TestingCode/PID_TEST_2/robot-config.h
@@ -0,0 +1,6 @@
+vex::brain Brain;
+vex::motor LeftDriveMotorOne = vex::motor(vex::PORT1, false);
+vex::motor LeftDriveMotorTwo = vex::motor(vex::PORT2, false);
+vex::motor RightDriveMotorOne = vex::motor(vex::PORT15, true);
+vex::motor RightDriveMotorTwo = vex::motor(vex::PORT13, true);
+vex::gyro Gyro = vex::gyro(Brain.ThreeWirePort.A);
