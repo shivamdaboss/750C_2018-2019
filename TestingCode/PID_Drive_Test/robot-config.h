89f65bfd7bdd8d16138bed9cbf37dd55398556ec
Shivam Agrawal <agrawalshivam1119@gmail.com>
2019-02-28 12:19:23 -0500

    Create robot-config.h

---
+++ TestingCode/PID_Drive_Test/robot-config.h
@@ -0,0 +1,9 @@
+vex::brain Brain;
+vex::motor LeftDriveOne = vex::motor(vex::PORT1, false);
+vex::motor LeftDriveTwo = vex::motor(vex::PORT2, false);
+vex::motor RightDriveOne = vex::motor(vex::PORT15, true);
+vex::gyro Gyro = vex::gyro(Brain.ThreeWirePort.A);
+vex::motor RightDriveTwo = vex::motor(vex::PORT4, true);
+
+
+
